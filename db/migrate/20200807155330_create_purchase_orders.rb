40c3096ce96af54aaf54969c609cd86b9700e46b
alinan-vn <jandrolinan@gmail.com>
2020-08-07 11:13:03 -0500

    added purchase orders and shopping carts tables

--- db/migrate/20200807155330_create_purchase_orders.rb
+++ db/migrate/20200807155330_create_purchase_orders.rb
@@ -1,6 +1,8 @@
 class CreatePurchaseOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :purchase_orders do |t|
+      t.integer :buyer_id
+      t.integer :product_id
 
       t.timestamps
     end
--- db/migrate/20200807155346_create_shopping_carts.rb
+++ db/migrate/20200807155346_create_shopping_carts.rb
@@ -1,6 +1,11 @@
 class CreateShoppingCarts < ActiveRecord::Migration[6.0]
   def change
     create_table :shopping_carts do |t|
+      t.integer :buyer_id
+      t.string :status
+      t.string :products
+      t.string :services
+      t.float :value
 
       t.timestamps
     end
