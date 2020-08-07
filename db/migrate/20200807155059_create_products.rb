a70ad16725c518fdb38894d3992fd08d34941cec
alinan-vn <jandrolinan@gmail.com>
2020-08-07 11:02:08 -0500

    created products and bookmarked products tables

--- db/migrate/20200807155059_create_products.rb
+++ db/migrate/20200807155059_create_products.rb
@@ -1,6 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
+      t.integer :seller_id
+      t.string :title
+      t.string :description
 
       t.timestamps
     end
--- db/migrate/20200807155158_create_bookmarked_products.rb
+++ db/migrate/20200807155158_create_bookmarked_products.rb
@@ -1,6 +1,8 @@
 class CreateBookmarkedProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :bookmarked_products do |t|
+      t.integer :buyer_id
+      t.integer :product_id
 
       t.timestamps
     end
