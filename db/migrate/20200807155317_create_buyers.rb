2a2c3b4c449349aca942d1a78932505d5712fcf7
alinan-vn <jandrolinan@gmail.com>
2020-08-07 11:05:24 -0500

    created buyers table

--- db/migrate/20200807155317_create_buyers.rb
+++ db/migrate/20200807155317_create_buyers.rb
@@ -1,6 +1,10 @@
 class CreateBuyers < ActiveRecord::Migration[6.0]
   def change
     create_table :buyers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :password_digest
+      t.string :email
 
       t.timestamps
     end
