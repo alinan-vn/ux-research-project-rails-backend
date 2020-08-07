288ce8bb1544aa27b16a700b2f10c26518b7d864
alinan-vn <jandrolinan@gmail.com>
2020-08-07 11:03:39 -0500

    added services and bookmarked services tables

--- db/migrate/20200807155216_create_services.rb
+++ db/migrate/20200807155216_create_services.rb
@@ -1,6 +1,9 @@
 class CreateServices < ActiveRecord::Migration[6.0]
   def change
     create_table :services do |t|
+      t.integer :seller_id
+      t.string :title
+      t.string :description
 
       t.timestamps
     end
--- db/migrate/20200807155304_create_bookmarked_services.rb
+++ db/migrate/20200807155304_create_bookmarked_services.rb
@@ -1,6 +1,8 @@
 class CreateBookmarkedServices < ActiveRecord::Migration[6.0]
   def change
     create_table :bookmarked_services do |t|
+      t.integer :buyer_id
+      t.integer :service_id
 
       t.timestamps
     end
