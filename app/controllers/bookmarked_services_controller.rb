7fc22cef8a12f7d42cdf805277db8683e35c2108
alinan-vn <jandrolinan@gmail.com>
2020-08-16 17:37:39 -0500

    bookmarked service controller setup

--- app/controllers/bookmarked_services_controller.rb
+++ app/controllers/bookmarked_services_controller.rb
@@ -1,74 +1,33 @@
 class BookmarkedServicesController < ApplicationController
-  before_action :set_bookmarked_service, only: [:show, :edit, :update, :destroy]
-
-  # GET /bookmarked_services
-  # GET /bookmarked_services.json
   def index
-    @bookmarked_services = BookmarkedService.all
+    render json: BookmarkedService.all
   end
 
-  # GET /bookmarked_services/1
-  # GET /bookmarked_services/1.json
   def show
+    bookmarked_service = BookmarkedService.find_by(id: params[:id])
+    render json: { buyer: bookmarked_service.buyer, service: bookmarked_service.service }
   end
 
-  # GET /bookmarked_services/new
-  def new
-    @bookmarked_service = BookmarkedService.new
-  end
-
-  # GET /bookmarked_services/1/edit
-  def edit
-  end
-
-  # POST /bookmarked_services
-  # POST /bookmarked_services.json
   def create
-    @bookmarked_service = BookmarkedService.new(bookmarked_service_params)
+    service = params[:service]
+    buyer = params[:buyer]
+    bookmarked_service = BookmarkedService.new(buyer: buyer, service: service)
 
-    respond_to do |format|
-      if @bookmarked_service.save
-        format.html { redirect_to @bookmarked_service, notice: 'Bookmarked service was successfully created.' }
-        format.json { render :show, status: :created, location: @bookmarked_service }
-      else
-        format.html { render :new }
-        format.json { render json: @bookmarked_service.errors, status: :unprocessable_entity }
-      end
+    if bookmarked_service.valid?
+      bookmarked_service.save
+      render json: { msg: "Bookmark for #{service.title} created!}" service: service, bookmarked_service: bookmarked_service }
+    else
+      render json: { error: 'Bookmark unsuccesful', attempt: params }
     end
   end
 
-  # PATCH/PUT /bookmarked_services/1
-  # PATCH/PUT /bookmarked_services/1.json
-  def update
-    respond_to do |format|
-      if @bookmarked_service.update(bookmarked_service_params)
-        format.html { redirect_to @bookmarked_service, notice: 'Bookmarked service was successfully updated.' }
-        format.json { render :show, status: :ok, location: @bookmarked_service }
-      else
-        format.html { render :edit }
-        format.json { render json: @bookmarked_service.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /bookmarked_services/1
-  # DELETE /bookmarked_services/1.json
   def destroy
-    @bookmarked_service.destroy
-    respond_to do |format|
-      format.html { redirect_to bookmarked_services_url, notice: 'Bookmarked service was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    bookmarked_service = BookmarkedService.delete(params[:id])
+    render json: bookmarked_service
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bookmarked_service
-      @bookmarked_service = BookmarkedService.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def bookmarked_service_params
-      params.fetch(:bookmarked_service, {})
+    def bookmarked_service_params(*args)
+      params.require(:bookmarked_service).permit(*args)
     end
 end
