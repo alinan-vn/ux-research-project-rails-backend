9448451e5b35b0313ecd67d04787b175a9140338
alinan-vn <jandrolinan@gmail.com>
2020-08-16 17:58:26 -0500

    product controller setup

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,74 +1,45 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
-
-  # GET /products
-  # GET /products.json
   def index
-    @products = Product.all
+    render json: Product.all
   end
 
-  # GET /products/1
-  # GET /products/1.json
   def show
+    product = Product.find_by(id: params[:id])
+    render json: { buyer: product.buyer, product: product }
   end
 
-  # GET /products/new
-  def new
-    @product = Product.new
-  end
-
-  # GET /products/1/edit
-  def edit
-  end
-
-  # POST /products
-  # POST /products.json
   def create
-    @product = Product.new(product_params)
+    product = params[:product]
+    buyer = params[:buyer]
+    product = Product.new(buyer: buyer, product: product)
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if product.valid?
+      product.save
+      render json: { msg: "Product: #{product.title} added!}" buyer: buyer, product: product }
+    else
+      render json: { error: 'Product could not be added.. :(', attempt: params }
     end
   end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
   def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    product = Product.find_by(id: params[:product][:id])
+    test_product = Product.new(product_params)
+
+    if test_product.valid?
+      product.update(product_params)
+      render json: { msg: "Product: #{product.title} successfully updated!", product: product }
+    else
+      render json: {error: "Update unsuccessful: #{product.title}", attempt: params}
     end
   end
 
-  # DELETE /products/1
-  # DELETE /products/1.json
   def destroy
-    @product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    product = Product.delete(params[:id])
+    render json: product
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.fetch(:product, {})
+    def product_params(*args)
+      params.require(:product).permit(*args)
     end
 end
