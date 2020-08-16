6cbc7c2fb836743b949131c3951f5ad60c5fe352
alinan-vn <jandrolinan@gmail.com>
2020-08-16 17:47:38 -0500

    service order controller setup

--- app/controllers/service_orders_controller.rb
+++ app/controllers/service_orders_controller.rb
@@ -1,74 +1,34 @@
 class ServiceOrdersController < ApplicationController
-  before_action :set_service_order, only: [:show, :edit, :update, :destroy]
-
-  # GET /service_orders
-  # GET /service_orders.json
   def index
-    @service_orders = ServiceOrder.all
+    render json: ServiceOrder.all
   end
 
-  # GET /service_orders/1
-  # GET /service_orders/1.json
   def show
+    service_order = ServiceOrder.find_by(id: params[:id])
+    render json: { buyer: service_order.buyer, service: service_order.service }
   end
 
-  # GET /service_orders/new
-  def new
-    @service_order = ServiceOrder.new
-  end
-
-  # GET /service_orders/1/edit
-  def edit
-  end
-
-  # POST /service_orders
-  # POST /service_orders.json
   def create
-    @service_order = ServiceOrder.new(service_order_params)
+    service = params[:service]
+    buyer = params[:buyer]
+    service_order = ServiceOrder.new(buyer: buyer, service: service)
 
-    respond_to do |format|
-      if @service_order.save
-        format.html { redirect_to @service_order, notice: 'Service order was successfully created.' }
-        format.json { render :show, status: :created, location: @service_order }
-      else
-        format.html { render :new }
-        format.json { render json: @service_order.errors, status: :unprocessable_entity }
-      end
+    if service_order.valid?
+      service_order.save
+      render json: { msg: "Service Order successful: #{service.title}!" service: service, service_order: service_order }
+    else
+      render json: { error: 'Bookmark unsuccessful', attempt: params }
     end
   end
 
-  # PATCH/PUT /service_orders/1
-  # PATCH/PUT /service_orders/1.json
-  def update
-    respond_to do |format|
-      if @service_order.update(service_order_params)
-        format.html { redirect_to @service_order, notice: 'Service order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @service_order }
-      else
-        format.html { render :edit }
-        format.json { render json: @service_order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /service_orders/1
-  # DELETE /service_orders/1.json
-  def destroy
-    @service_order.destroy
-    respond_to do |format|
-      format.html { redirect_to service_orders_url, notice: 'Service order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # Probably not needed for this table
+  # def destroy
+  #   service_order = ServiceOrder.delete(params[:id])
+  #   render json: service_order
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_service_order
-      @service_order = ServiceOrder.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def service_order_params
-      params.fetch(:service_order, {})
+    def service_order_params(*args)
+      params.require(:service_order).permit(*args)
     end
 end
