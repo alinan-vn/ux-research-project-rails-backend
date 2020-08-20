a96ce2f4f8b8a729515bda258ebf87051f9b80e2
alinan-vn <jandrolinan@gmail.com>
2020-08-20 16:15:38 -0500

    seller validations

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -4,4 +4,7 @@ class Seller < ApplicationRecord
     has_many :products
     has_many :services
 
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
 end
