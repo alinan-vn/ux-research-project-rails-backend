eac78bb0bf66dee69c822fd9789aad2169e6f7fb
alinan-vn <jandrolinan@gmail.com>
2020-08-20 16:15:13 -0500

    buyer validations

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -8,4 +8,8 @@ class Buyer < ApplicationRecord
     has_many :product_orders
     has_many :service_orders
     has_many :shopping_carts
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
 end
