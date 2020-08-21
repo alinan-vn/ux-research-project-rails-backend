0f8b1cf79d40bd67d23f29a3123eca680fc73902
alinan-vn <jandrolinan@gmail.com>
2020-08-20 20:51:18 -0500

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
     belongs_to :seller
+
+    validates :seller, presence: true
+    validates :title, presence: true
+    validates :title, length: { maximum: 100 }
+    validates :description, presence: true
+    validates :description, length: {maximum: 1000 }
 end
